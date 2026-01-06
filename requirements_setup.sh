377fe850592c9a490285e6a4410d1c7ad9c12e36
nrastija <niko.rastija@gmail.com>
2026-01-06 15:24:43 +0100

    feat: add requirements_setup.sh bash script with Flora-2 installation
    - Flora-2 version 2.1. (currently latest)

---
+++ requirements_setup.sh
@@ -0,0 +1,22 @@
+#
+# Skripta za setup okruženja
+# Datum: 6/1/2026
+#
+
+
+echo "============================================"
+echo "[1/x] Instaliranje Flora-2 verzija 2.1."
+echo "============================================"
+
+# Instalacija preuzeta od dokumenta Installfest sa 2. laboratorijskih vježbi na predmetu
+wget -O flora2.run "https://downloads.sourceforge.net/project/flora/FLORA-2/2.1%20%28Punica%20granatum%29/flora2-2.1-RC1.run?ts=gAAAAABnDtrFOdlvUMvPBO_22hsX9guXx4pgiB2FfNTXZlfYrHjRXsGw3WUatICxM2HuzXnYmkb7J8qaoN8Xawt5e-8R7BZqww%3D%3D&use_mirror=master&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fflora%2Ffiles%2FFLORA-2%2F2.1%2520%2528Punica%2520granatum%2529%2F"
+chmod +x flora2.run
+./flora2.run
+
+rm flora2.run
+
+
+
+echo "============================================"
+echo "Završena setup skripta!"
+echo "============================================"
